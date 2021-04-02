1a80bfa463d02d134b2a6e05ec03f1148502772a
DiamondGamerA <61519876+DiamondGamerA@users.noreply.github.com>
2021-04-01 23:36:34 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 console.log('hello world')
+import dotenv from 'dotenv'
+import Discord from 'discord.js'
+dotenv.config()
+const client = new Discord.Client()
+client.login(process.env.TOKEN)
+client.on('ready', () => {
+  console.log('Ready!')
+})
+client.on('https://media1.giphy.com/media/rWAF3CyEQEzvhrNtmy/giphy.gif', (msg) => {
+  const channel = msg.channel as Discord.TextChannel
+  channel.send('Hi there!')
+})
